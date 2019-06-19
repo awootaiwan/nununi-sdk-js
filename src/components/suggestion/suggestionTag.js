89ad165cf7eb9e05b40913dcea9ed6e99bf17215
johnnyyu0930 <awoo.johnnyyu@gmail.com>
2019-06-19 17:34:05 +0800

    fix tab to 2 space

--- src/components/suggestion/suggestionTag.js
+++ src/components/suggestion/suggestionTag.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const SuggestionTag = ({ link, text }) => {
-	return (<a href={link}>
-		<span>
-			{text}
-		</span>
-	</a>)
-}
+  return (
+    <a href={link}>
+      <span>{text}</span>
+    </a>
+  );
+};
 
 export default SuggestionTag;
