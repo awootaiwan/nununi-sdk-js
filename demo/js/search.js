e70c67621efae5533cd07e0707cd2f5d48b93b82
mingyou1219 <awoo.mingyou@gmail.com>
2019-06-21 16:59:34 +0800

    add effect and change js sure-button

--- demo/js/search.js
+++ demo/js/search.js
@@ -1,6 +1,8 @@
 const baseUrl = `${location.protocol}//${location.host}?menu=`
-$('.sure-bottom').on('click', function(){
+$('.sure-botton').on('click', function(){
     val = $(".search-input").val();
     url = `${baseUrl}${val}`
-    location.href = url
+    if(!val == "" || val == NULL) {
+        location.href = url
+    }
 })
