e8593f4cb33eade8d7fc022cada7b36612ee661e
mingyou1219 <awoo.mingyou@gmail.com>
2019-06-21 11:28:17 +0800

    add header-top and change search.js menu

--- demo/js/search.js
+++ demo/js/search.js
@@ -1,4 +1,4 @@
-const baseUrl = `${location.protocol}//${location.host}?tags=`
+const baseUrl = `${location.protocol}//${location.host}?menu=`
 $('.sure-bottom').on('click', function(){
     val = $(".search-input").val();
     url = `${baseUrl}${val}`
