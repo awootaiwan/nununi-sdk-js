74869ce3ee2a3f172daf7232b6094a7bf6b0c29a
mingyou1219 <awoo.mingyou@gmail.com>
2019-06-24 11:27:58 +0800

    first modify search bug

--- demo/js/search.js
+++ demo/js/search.js
@@ -2,7 +2,8 @@ const baseUrl = `${location.protocol}//${location.host}?menu=`
 $('.sure-botton').on('click', function(){
     val = $(".search-input").val();
     url = `${baseUrl}${val}`
-    if(!val == "" || val == NULL) {
+
+    if ( val.trim() != "" ) {
         location.href = url
     }
 })
