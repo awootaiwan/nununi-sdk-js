3d00358f22d5a11f4e3454b320ab6494200d9979
mingyou1219 <awoo.mingyou@gmail.com>
2019-06-24 12:03:07 +0800

    change img root

--- demo/js/search.js
+++ demo/js/search.js
@@ -2,7 +2,6 @@ const baseUrl = `${location.protocol}//${location.host}?menu=`
 $('.sure-botton').on('click', function(){
     val = $(".search-input").val();
     url = `${baseUrl}${val}`
-
     if ( val.trim() != "" ) {
         location.href = url
     }
