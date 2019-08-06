0ef1c7d42602cbfe4224742d1ff2755369fb6214
Acaki <axsz4298@gmail.com>
2019-08-06 17:24:07 +0800

    Fix ugly code NC-360 #time 2h

--- src/api/base.js
+++ src/api/base.js
@@ -1,36 +1,36 @@
 import axios from 'axios';
 
-const getApiData = async(id = process.env.NUNUNI_ID, token = process.env.NUNUNI_TOKEN, version, pageInfo)=> {
+const getApiData = async (
+  id = process.env.NUNUNI_ID, token = process.env.NUNUNI_TOKEN, version, pageInfo,
+) => {
   const url = `${process.env.NUNUNI_DOMAIN}/nununi/${version}/${id}/${process.env.NUNUNI_APINAME}/`;
   const tags = pageInfo.tags.split(',');
-  const page = pageInfo.page;
-  const limit = pageInfo.limit;
-  const sort = pageInfo.sort;
+  const { page, limit, sort } = pageInfo;
 
-  let data = {
+  const data = {
     tags,
     page,
     limit,
-    sort
+    sort,
   };
 
-  let headers = {
-    'Authorization' : `Bearer ${token}`
-  }
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
 
-  let {status,data:response} = await axios.post(url, data, {headers});
-  
-  if (status != 200 ) {
+  let { status, data: response } = await axios.post(url, data, { headers });
+
+  // TODO: Rewrite this nonsense
+  if (status !== 200) {
     response = {
-      'errmsg' : response.error_description,
-      'errcode' : status,
-      ...response
-    } 
-    delete response.error_description
+      errmsg: response.error_description,
+      errcode: status,
+      ...response,
+    };
+    delete response.error_description;
   }
-  return response;
-}
 
-export {
-  getApiData
-}
+  return response;
+};
+
+export default getApiData;
--- src/app.js
+++ src/app.js
@@ -1,47 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { getApiData } from './api/base';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getApiData from './api/base';
 import ErrorAlert from './components/erroralert/ErrorAlert';
 import Suggestion from './components/suggestion/suggestion';
 import ProductList from './components/product/ProductList';
 
-const getContent = async (id, token, version) => {
-  let urlParms = getUrlParms();
-  let data = {
-    errcode: 0,
-    errmsg: "",
-    result: {}
-  }
-
-  data = (urlParms.tags) ? await getApiData(id, token, version, urlParms) : data ;
-
-  return data;
-}
-
 const getUrlParms = () => {
-  let url = new URL(window.location.href);
-  let tags = url.searchParams.get("tags");
-  let page = url.searchParams.get("page");
-  let sort = url.searchParams.get("sort");
-  let limit = url.searchParams.get("limit");
+  const url = new URL(window.location.href);
+  const tags = url.searchParams.get('tags');
+  let page = url.searchParams.get('page');
+  let sort = url.searchParams.get('sort');
+  let limit = url.searchParams.get('limit');
 
   if (page === null) {
     page = 1;
   }
-  if (sort === null || sort === "") {
+  if (sort === null || sort === '') {
     sort = 8;
   }
   if (limit === null) {
-    limit = 10 ;
+    limit = 10;
   }
-  let data = {
+  return {
     tags,
     page,
     limit,
-    sort
-  }
+    sort,
+  };
+};
+
+const getContent = async (id, token, version) => {
+  const urlParms = getUrlParms();
+  let data = {
+    errcode: 0,
+    errmsg: '',
+    result: {},
+  };
+
+  data = (urlParms.tags) ? await getApiData(id, token, version, urlParms) : data;
+
   return data;
-}
+};
 
 const App = (props) => (
   <React.Fragment>
@@ -51,7 +50,7 @@ const App = (props) => (
       <ErrorAlert errmsg={props.errmsg} />
     )}
   </React.Fragment>
-)
+);
 
 let ID = '';
 let TOKEN = '';
@@ -61,8 +60,8 @@ const CupidSDK = {
     ID = id;
     TOKEN = token;
     if (!ID || !TOKEN) {
-      throw new Error("nununi id 或者 access token 未填寫");
-    } 
+      throw new Error('nununi id 或者 access token 未填寫');
+    }
   },
   setAPIVersion: async (version) => {
     APIVER = version;
@@ -71,31 +70,43 @@ const CupidSDK = {
     if (!ID || !TOKEN) {
       throw new Error('請先呼叫函數 window.CupidSDK.init');
     }
-    const CupidSuggestionTag = document.getElementById("cupid-suggestion-tag");
+    const CupidSuggestionTag = document.getElementById('cupid-suggestion-tag');
     if (!CupidSuggestionTag || CupidSuggestionTag.length < 1) {
       throw new Error('請先加入 <div id="cupid-suggestion-tag"></div> HTML標籤');
     }
-    let data = await getContent(ID, TOKEN, APIVER);
+    const data = await getContent(ID, TOKEN, APIVER);
     const pageInfo = getUrlParms();
     const { result, errcode, errmsg } = data;
 
-    ReactDOM.render(<App errcode={errcode} errmsg={errmsg}><Suggestion suggestionTags={result.suggestionTags} pageInfo={pageInfo} /></App>, CupidSuggestionTag);    
+    ReactDOM.render(
+      <App errcode={errcode} errmsg={errmsg}>
+        <Suggestion
+          suggestionTags={result.suggestionTags}
+          pageInfo={pageInfo}
+        />
+      </App>, CupidSuggestionTag,
+    );
   },
   renderProductList: async () => {
     if (!ID || !TOKEN) {
       throw new Error('請先呼叫函數 window.CupidSDK.init');
     }
-    const CupidProductList = document.getElementById("cupid-product-list");
+    const CupidProductList = document.getElementById('cupid-product-list');
     if (!CupidProductList || CupidProductList.length < 1) {
       throw new Error('請先加入 <div id="cupid-product-list"></div> HTML標籤');
-    } 
-    
-    let data = await getContent(ID, TOKEN, APIVER);
+    }
+
+    const data = await getContent(ID, TOKEN, APIVER);
     const pageInfo = getUrlParms();
     const { errcode, errmsg } = data;
-    
-    ReactDOM.render(<App errcode={errcode} errmsg={errmsg}><ProductList productlist={data.result} pageInfo={pageInfo} /></App>, CupidProductList);    
-  }
-}
+
+    ReactDOM.render(
+      <App errcode={errcode} errmsg={errmsg}>
+        <ProductList productlist={data.result} pageInfo={pageInfo} />
+      </App>,
+      CupidProductList,
+    );
+  },
+};
 
 module.exports = window.CupidSDK = CupidSDK;
