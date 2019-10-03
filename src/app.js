98f546aaabe71384188658ce0a6c5c74b589d4ce
allenlin <allenlin@awoo.com.tw>
2019-10-03 11:56:38 +0800

    create api/config.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {
   getApiData,
   getProductTagApiData,
   getClassifyApiData
-} from "./api/base";
-import ErrorAlert from "./components/erroralert/ErrorAlert";
-import Suggestion from "./components/suggestion/suggestion";
-import ProductList from "./components/product/ProductList";
-import ProductTag from "./components/productTag/productTag";
+} from './api/base';
+import ErrorAlert from './components/erroralert/ErrorAlert';
+import Suggestion from './components/suggestion/suggestion';
+import ProductList from './components/product/ProductList';
+import ProductTag from './components/productTag/productTag';
 
-const splitTags = tags => (typeof tags === "string" ? tags.split(",") : tags);
+const splitTags = tags => (typeof tags === 'string' ? tags.split(',') : tags);
 
 const App = props => (
   <React.Fragment>
@@ -25,11 +25,11 @@ const App = props => (
 class CupidSDK {
   constructor(id = process.env.NUNUNI_ID) {
     if (!id || id.length < 1) {
-      throw new Error("nununi id is not setting");
+      throw new Error('nununi id is not setting');
     }
     this.id = id;
-    this.contentApiVer = "latest";
-    this.productsApiVer = "latest";
+    this.contentApiVer = 'latest';
+    this.productsApiVer = 'latest';
     this.limit = 10;
   }
 
@@ -42,27 +42,27 @@ class CupidSDK {
   }
 
   setLimit(limit) {
-    if (typeof limit != "number") {
-      throw Error("setLimit is not number.");
+    if (typeof limit != 'number') {
+      throw Error('setLimit is not number.');
     }
 
     if (limit < 1) {
-      throw Error("limit need to be greater than 0.");
+      throw Error('limit need to be greater than 0.');
     }
     this.limit = limit;
   }
 
   _getUrlParms() {
     const url = new URL(window.location.href);
-    const tags = url.searchParams.get("tags");
-    let page = url.searchParams.get("page");
-    let sort = url.searchParams.get("sort");
-    let limit = url.searchParams.get("limit");
+    const tags = url.searchParams.get('tags');
+    let page = url.searchParams.get('page');
+    let sort = url.searchParams.get('sort');
+    let limit = url.searchParams.get('limit');
 
     if (page === null) {
       page = 1;
     }
-    if (sort === null || sort === "") {
+    if (sort === null || sort === '') {
       sort = 8;
     }
     if (limit === null) {
@@ -92,7 +92,7 @@ class CupidSDK {
       {
         tags: splitTags(tags)
       },
-      "pageInfo"
+      'pageInfo'
     );
   }
 
@@ -103,7 +103,7 @@ class CupidSDK {
       {
         tags: splitTags(tags)
       },
-      "suggestionTags"
+      'suggestionTags'
     );
   }
 
@@ -117,12 +117,12 @@ class CupidSDK {
         sort,
         limit
       },
-      "products"
+      'products'
     );
   }
 
   async renderSuggestionTag() {
-    const CupidSuggestionTag = document.getElementById("cupid-suggestion-tag");
+    const CupidSuggestionTag = document.getElementById('cupid-suggestion-tag');
     if (!CupidSuggestionTag || CupidSuggestionTag.length < 1) {
       throw new Error(
         '請先加入 <div id="cupid-suggestion-tag"></div> HTML標籤'
@@ -149,7 +149,7 @@ class CupidSDK {
   }
 
   async renderProductList() {
-    const CupidProductList = document.getElementById("cupid-product-list");
+    const CupidProductList = document.getElementById('cupid-product-list');
     if (!CupidProductList || CupidProductList.length < 1) {
       throw new Error('請先加入 <div id="cupid-product-list"></div> HTML標籤');
     }
@@ -176,20 +176,20 @@ class CupidSDK {
   }
 
   async renderProductTag(productId) {
-    const CupidProductTag = document.getElementById("cupid-product-tag");
+    const CupidProductTag = document.getElementById('cupid-product-tag');
     if (!CupidProductTag || CupidProductTag.length < 1) {
       throw new Error('請先加入 <div id="cupid-product-tag"></div> HTML標籤');
     }
     if (productId === undefined) {
       const idDom = document.querySelector(
-        "div[data-cupid-product-id], a[data-cupid-product-id], span[data-cupid-product-id]"
+        'div[data-cupid-product-id], a[data-cupid-product-id], span[data-cupid-product-id]'
       );
       if (!idDom) {
         throw new Error(
-          "請在div或a或span標籤內增加data-cupid-product-id屬性，並指定商品id"
+          '請在div或a或span標籤內增加data-cupid-product-id屬性，並指定商品id'
         );
-      } else if (idDom.dataset.cupidProductId === "") {
-        throw new Error("data-cupid-product-id屬性為空值");
+      } else if (idDom.dataset.cupidProductId === '') {
+        throw new Error('data-cupid-product-id屬性為空值');
       }
       productId = idDom.dataset.cupidProductId;
     }
@@ -207,7 +207,7 @@ class CupidSDK {
 
   getClassify(productIdArray) {
     if (productIdArray.length < 1) {
-      throw new Error("傳入商品id陣列為空陣列");
+      throw new Error('傳入商品id陣列為空陣列');
     }
     return getClassifyApiData(this.id, this.productsApiVer, {
       productIds: productIdArray
@@ -215,7 +215,7 @@ class CupidSDK {
   }
 
   async renderClassify(productId) {
-    const CupidClassify = document.getElementById("cupid-classify");
+    const CupidClassify = document.getElementById('cupid-classify');
     if (!CupidClassify || CupidClassify.length < 1) {
       throw new Error('請先加入 <div id="cupid-classify"></div> HTML標籤');
     }
@@ -223,12 +223,12 @@ class CupidSDK {
     if (productId === undefined) {
       const idDom = [
         ...document.querySelectorAll(
-          "div[data-cupid-product-id], a[data-cupid-product-id], span[data-cupid-product-id]"
+          'div[data-cupid-product-id], a[data-cupid-product-id], span[data-cupid-product-id]'
         )
       ];
       if (!idDom) {
         throw new Error(
-          "請在div或a或span標籤內增加data-cupid-product-id屬性，並指定商品id"
+          '請在div或a或span標籤內增加data-cupid-product-id屬性，並指定商品id'
         );
       }
       let productIdArray = idDom.map(item => {
@@ -250,13 +250,13 @@ class CupidSDK {
 }
 /** Detect free variable `global` from Node.js. */
 const freeGlobal =
-  typeof global == "object" && global && global.Object === Object && global;
+  typeof global == 'object' && global && global.Object === Object && global;
 
 /** Detect free variable `self`. */
 const freeSelf =
-  typeof self == "object" && self && self.Object === Object && self;
+  typeof self == 'object' && self && self.Object === Object && self;
 
 /** Used as a reference to the global object. */
-const root = freeGlobal || freeSelf || Function("return this")();
+const root = freeGlobal || freeSelf || Function('return this')();
 
 module.exports = root.CupidSDK = CupidSDK;
