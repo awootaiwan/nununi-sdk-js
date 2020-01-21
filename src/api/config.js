b9afb72428845574940cc318812ac3af1d1eb053
Chi Caesar <chicaesar@Chide-MacBook-Pro.local>
2020-01-22 07:11:40 +0800

    set ajax default timeout

--- src/api/config.js
+++ src/api/config.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: `${process.env.NUNUNI_DOMAIN}/nununi`,
+  timeout: 1000,
   headers: {
     'Content-Type': 'application/json'
   }
