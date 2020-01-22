70f61fd512b999901f7966bd77b835a261fd9777
Chi Caesar <chicaesar@Chide-MacBook-Pro.local>
2020-01-22 14:34:31 +0800

    remove timeout for api

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: `${process.env.NUNUNI_DOMAIN}/nununi`,
-  timeout: 1000,
   headers: {
     'Content-Type': 'application/json'
   }
