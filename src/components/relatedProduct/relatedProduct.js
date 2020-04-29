b3c6b512a6f5714864709da8e09bccc48a2b6857
“marionLove” <marionma@awoo.com.tw>
2020-04-29 15:13:38 +0800

    use React Api

--- src/components/relatedProduct/relatedProduct.js
+++ src/components/relatedProduct/relatedProduct.js
@@ -1,72 +1,68 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useTranslation } from 'react-i18next';
 import RelatedProductItem from "./relatedProductItem.js";
 import styled from 'styled-components';
 import Carousel, { consts } from 'react-elastic-carousel';
 
 const RelatedProductWrapper = styled.div`
-.nununi-related-product {
-  position: relative;
-  width: 100%;
-  a {
-    width: 100%;
+&.nununi-related {
+  &-wrapper {
+    p {
+      font-size: 15px;
+      text-align: left;
+    }
+    .rec-slider-container {
+      margin: 0 20px;
+    }
+    .rec-pagination {
+      display:none;
+    }
+  }
+}
+.nununi-related {
+  &-product {
     position: relative;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: block;
-    :hover {
-      cursor: pointer;
-    }
-  }
-  &-img {
-    display: block;
-    border-radius: 2px;
-    :hover {
-      cursor: pointer;
-    }
-    div {
-      padding-bottom: 100%;
-      background-repeat: no-repeat;
-      background-position: center;
-      background-color: #fff;
-      background-size: 100%;
-    }
-  }
-  &-name {
-    margin-top: 12px;
-    overflow: hidden;
-    height: 32px;
-    line-height: 16px;
-    letter-spacing: .05em;
-    text-overflow: ellipsis;
-  }
-}
-
-p {
-  font-size: 15px;
-  text-align: left;
-}
-.nununi-related-carousel {
-  position: relative;
-  &-paginition {
-    padding: 20px 0;
-    div { 
-      display: inline-block;
-      width: 25px;
-      height: 8px;
-      margin: 0 5px;
-      background-color: #ccc;
-      border-radius: 3px;
+    width: 100%;
+    a {
+      width: 100%;
+      position: relative;
+      overflow: hidden;
+      box-sizing: border-box;
+      display: block;
       :hover {
         cursor: pointer;
       }
     }
-    div[active=true] {
-      background-color: #aaa;
+    &-img {
+      display: block;
+      border-radius: 2px;
+      :hover {
+        cursor: pointer;
+      }
+      div {
+        padding-bottom: 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-color: #fff;
+        background-size: 100%;
+      }
+    }
+    &-name {
+      margin-top: 12px;
+      overflow: hidden;
+      max-height: 48px;
+      min-height: 32px
+      line-height: 16px;
+      letter-spacing: .05em;
+      text-overflow: ellipsis;
     }
   }
+  &-carousel {
+    position: relative;
+  }
 }
-.carousel-arrow {
+
+.nununi-carousel-arrow {
   position: absolute;
   top: 50%;
   width: 20px;
@@ -91,17 +87,7 @@ p {
     right: 0;
   }
 }
-.rec-slider-container {
-  margin: 0 20px;
-}
-.rec-pagination {
-  display:none;
-}
-`;
-function slideArrow({ type, onClick }) {
-  const pointer = type === consts.PREV ? '<' : '>';
-  return <div className={`carousel-arrow ${type}`} onClick={onClick}>{pointer}</div>
-};
+`
 
 const RelatedProduct = ({ RelatedProduct }) => {
   const { t } = useTranslation();
@@ -120,8 +106,22 @@ const RelatedProduct = ({ RelatedProduct }) => {
       })
     })
   }
+  function slideArrow({ type, onClick }) {
+    const pointer = type === consts.PREV ? '<' : '>';
+    return <div 
+              className={`nununi-carousel-arrow ${type}`} 
+              onClick={onClick}
+              ref={type === "NEXT" ? arrowNEXT : arrowPREV}>
+                {pointer}
+            </div>
+  };
+  const arrowNEXT = useRef(null);
+  const arrowPREV = useRef(null);
+  const [childNum, setChildNum] = useState(0);
+  const [showItem, setShowItem] = useState(0);
+  const [scrollItem, setScrollItem] = useState(0);
   return (
-    <RelatedProductWrapper className="nununi-related-wrapper" >
+    <RelatedProductWrapper className="nununi-related-wrapper">
       { productList && 
         productList.length > 0 &&
         <>
@@ -139,39 +139,32 @@ const RelatedProduct = ({ RelatedProduct }) => {
             className={'nununi-related-carousel'}
             renderArrow={slideArrow}
             ref={ref => {
-              const childrenLength = ref.props.children.length;
-              const carousel = document.querySelector('.nununi-related-carousel');
-              carousel.dataset.childNum = childrenLength;
+              if (ref !== null){ setChildNum(ref.props.children.length)}
             }}
             onResize={(currentBreakPoint) => {
               const idealHeight = document.querySelector('.nununi-related-product').clientHeight;
-              const adjustDom = document.querySelector('.rec-carousel');
+              const adjustDom = document.querySelector('.nununi-related-wrapper .rec-carousel');
               adjustDom.style.height = idealHeight + 'px';
-              const carousel = document.querySelector('.nununi-related-carousel');
-              carousel.dataset.scrollItem = currentBreakPoint.itemsToScroll;
-              const showItem = carousel.dataset.showItem = currentBreakPoint.itemsToShow;
-              const childNum = carousel.dataset.childNum;
+              setScrollItem(currentBreakPoint.itemsToScroll);
+              setShowItem(currentBreakPoint.itemsToShow)
               if (parseInt(showItem) > parseInt(childNum)) {
-                document.querySelector('.carousel-arrow.NEXT').style.display = 'none';
-                document.querySelector('.carousel-arrow.PREV').style.display = 'none';
+                arrowNEXT.current.style.display = 'none';
+                arrowPREV.current.style.display = 'none';
               }
             }}
             onPrevStart={(currentItem, nextItem) =>
               {
-                document.querySelector('.carousel-arrow.NEXT').style.display = 'block';
+                arrowNEXT.current.style.display = 'block';
                 if (nextItem.index == 0) {
-                  document.querySelector('.carousel-arrow.PREV').style.display = 'none';
+                  arrowPREV.current.style.display = 'none';
                 }
               }
             }
             onNextStart={(currentItem, nextItem) =>
               {
-                const carousel = document.querySelector('.nununi-related-carousel');
-                const scrollItem = parseInt(carousel.dataset.scrollItem);
-                const totalItem = parseInt(carousel.dataset.childNum);
-                document.querySelector('.carousel-arrow.PREV').style.display = 'block';
-                if (nextItem.index == (totalItem -1) || nextItem.index + scrollItem >= (totalItem)) {
-                  document.querySelector('.carousel-arrow.NEXT').style.display = 'none';
+                arrowPREV.current.style.display = 'block';
+                if (nextItem.index == (childNum -1) || nextItem.index + scrollItem >= (childNum)) {
+                  arrowNEXT.current.style.display = 'none';
                 }
               }
             }
